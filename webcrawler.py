d003bc64cda0fcd6867edb76826e193220c8137f
patjlouis <patjlouis@gmail.com>
2017-03-22 17:17:40 -0400

    created "startup.py" file and testing of wiki library

--- webcrawler.py
+++ webcrawler.py
@@ -1,6 +1,9 @@
+import wikipedia
+
 class webcrawler(object):
-    """This is where the algorithim that will habitate"""
+    """This is where the algorithim that will find relational information will habitate"""
 
 
+    print(wikipedia.search("Google"))
 
 
