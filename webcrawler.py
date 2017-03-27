cf0fdea4b491a442e12fb69ace033cc29be6cce7
patjlouis <patjlouis@gmail.com>
2017-03-27 15:15:35 -0400

    Updated summary function

--- webcrawler.py
+++ webcrawler.py
@@ -3,12 +3,12 @@ import wikipedia
 class webcrawler(object):
     """Algorithm that will find relational information will habitate"""
 
-def search_algorithm():
+def summary_algorithm():
     output = input("What would you like to search? : ")
     get_summary = lambda output: wikipedia.summary(output).encode('utf-8').strip()
     print(get_summary(output))
     
-search_algorithm()
+summary_algorithm()
 
 
 #try:
