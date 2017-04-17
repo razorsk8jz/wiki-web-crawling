e55c461e99890a99dab4582481bc5e755150ae6d
razorsk8jz <razorsk8jz@gmail.com>
2017-04-17 18:52:59 -0400

    Update items.py

--- Scrapy Version/wikiCrawler/wikiCrawler/items.py
+++ Scrapy Version/wikiCrawler/wikiCrawler/items.py
@@ -1,12 +1,6 @@
-# -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
+#create variables for the items we want to scrape
 class WikicrawlerItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
